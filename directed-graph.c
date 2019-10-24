400fce1455d889ae392d877d9f30955d8bcad0c6
Guadalupe Bernal <guadabernal@gmail.com>
2019-10-23 23:02:31 -0400

    Fixed header file changed overall structure, wrote print function

--- directed-graph.c
+++ directed-graph.c
@@ -12,11 +12,9 @@
 //------------------------------------------------------------------------
 // Construct the graph_int_t and initialize the fields inside.
 
-void graph_int_construct( list_int_t* this )
+void graph_int_construct( graph_int_t* this )
 {
   this->nodes_size = 0;
-  this->node_list = NULL;
-
   this->source_node = NULL;
   this->sink_node = NULL;
 }
@@ -26,45 +24,42 @@ void graph_int_construct( list_int_t* this )
 //------------------------------------------------------------------------
 // Destruct the graph_int_destruct, freeing any dynamically allocated memory
 
-void graph_int_destruct( list_int_t* this )
+void graph_int_destruct( graph_int_t* this )
 {
   // free the edges inside of each node
   for( int i = 0; i < this->nodes_size; i++) {
-    free(this->node_list[i]->e_list)
+    free(this->node_list[i]->e_list);
   }
 
   // free the list of nodes
-  free ( node_list );
+  free ( this->node_list );
 
   // Reset values in this
   this->nodes_size = 0;
-  this->node_list = NULL;
-
   this->source_node = NULL;
   this->sink_node = NULL;
 }
 
 //------------------------------------------------------------------------
-// list_int_size
+// graph_int_size
 //------------------------------------------------------------------------
-// Get the number of elements in the list.
+// Get the number of nodes in the graph.
 
-size_t list_int_size( list_int_t* this )
+size_t graph_int_size( graph_int_t* this )
 {
-  return this->size;
+  return this->nodes_size;
 }
 
 //------------------------------------------------------------------------
-// list_int_push_back
+// graph_int_push_back
 //------------------------------------------------------------------------
-// Push a new value into the list_int_t.
+// Push a new node into the graph
 
-void list_int_push_back( list_int_t* this, int value )
+void graph_int_push_back( graph_int_t* this, int value )
 {
   // Creates a new node and assigns its 'value' field value
   node_t* new_node = ( node_t* ) ece2400_malloc ( sizeof ( node_t ) );
   new_node->value = value;
-
 
   // Assign values to the new node's pointer variables. If the tail_ptr is not null
   // then tail_ptr->next_ptr must be re-assigned before the tail_ptr is moved to the new_node
@@ -92,62 +87,49 @@ void list_int_push_back( list_int_t* this, int value )
 // Pointer chase and return the value at the given index
 // If the index is out of bound, then return 0.
 
-int list_int_at( list_int_t* this, size_t idx )
-{
-  // Checks if the index is larger than the size and returns 0
-  if( idx >= this->size ) {
-    return 0;
-  }
-
-  // Iterates through idx nodes with the temp variable and returns
-  // the value at the idx index
-  node_t* temp;
-
-  temp = this->head_ptr;
-  for( size_t i = 0; i < idx; i++ ){
-    temp = temp->next_ptr;
-  }
-
-  return (int)temp->value;
-}
+// int list_int_at( list_int_t* this, size_t idx )
+// {
+//   // Checks if the index is larger than the size and returns 0
+//   if( idx >= this->size ) {
+//     return 0;
+//   }
+//
+//   // Iterates through idx nodes with the temp variable and returns
+//   // the value at the idx index
+//   node_t* temp;
+//
+//   temp = this->head_ptr;
+//   for( size_t i = 0; i < idx; i++ ){
+//     temp = temp->next_ptr;
+//   }
+//
+//   return (int)temp->value;
+// }
 
 //------------------------------------------------------------------------
-// list_int_find
+// graph_int_print
 //------------------------------------------------------------------------
-// Search the list for a value and return whether a value is found or not.
-// Returning 1 means found, and 0 means not found.
+// Print out the content of graph_int_t.
 
-int list_int_find( list_int_t* this, int value )
+void graph_int_print( graph_int_t* this )
 {
-  node_t* temp = this->head_ptr;
-
-  // if the value is found return 1, else traverse to the next node until
-  // the null node is reached, then return 0
-  while ( temp != NULL ) {
-    if ( (int)temp->value == value ) {
-      return 1;
+  // Iterrate through the nodes in list printing their value and  edges
+  for( size_t i = 0; i < this->nodes_size; i++ ){
+    printf ( "    Node : %s  Value: %zu \n", this->node_list[i]->name,
+        this->node_list[i]->value );
+    for( size_t j = 0; j < this->node_list[i]->edges; j++ ){
+      printf ( "        Edge from: %s  To: %s Cost: %zu \n",
+          this->node_list[i]->name,
+          this->node_list[i]->e_list[j]->next->name,
+          this->node_list[i]->e_list[j]->cost );
     }
-    temp = temp->next_ptr;
   }
-
-  return 0;
+  printf("\n");
 }
 
-//------------------------------------------------------------------------
-// list_int_print
-//------------------------------------------------------------------------
-// Print out the content of list_int_t.
 
-void list_int_print( list_int_t* this )
-{
-  // Use a temp variable to help itterate through the list
-  node_t* temp = this->head_ptr;
 
-  // Iterrate through the nodes printing their value until the node is null
-  while ( temp != NULL ) {
-    printf ( "%zu  ", temp->value );
-    temp = temp->next_ptr;
-  }
+int main(){
+  printf("\n\nhey you made it!!\n ");
 
-  printf("\n");
 }
--- directed-graph.h
+++ directed-graph.h
@@ -8,32 +8,33 @@
 #ifndef DIRECTED_GRAPH_H
 #define DIRECTED_GRAPH_H
 
-typedef struct _node_t {
-  size_t  value;  // value of the node
-  size_t  edges;  // number of edges
-  edge_t* e_list[];
-}
-node_t;
+struct node_t;
+struct edge_t;
+struct graph_int_t;
 
-typedef struct _edge_t {
+typedef struct node_t {
+  size_t          value;  // value of the node
+  size_t          edges;  // number of edges
+  char            name[255];
+  struct edge_t*  e_list[];
+} node_t;
+
+typedef struct edge_t {
   size_t  cost;
   node_t* next;
-}
-edge_t;
+} edge_t;
 
-typedef struct {
+typedef struct graph_int_t{
   size_t  nodes_size;
-  node_t* node_list[];
-
   node_t* source_node;
   node_t* sink_node;
-}
-graph_int_t;
+  node_t* node_list[];
+} graph_int_t;
 
 void   graph_int_construct ( graph_int_t* this );
 void   graph_int_destruct  ( graph_int_t* this );
 size_t graph_int_size      ( graph_int_t* this );
-void   graph_int_push_back ( graph_int_t* this, size_t value, int cost );
+void   graph_int_push_back ( graph_int_t* this, size_t value );
 
 int    graph_int_at        ( graph_int_t* this, size_t idx );
 int    graph_int_find      ( graph_int_t* this, size_t value );
